23096e1a460b455a0530b6b6cf9e0243a4df5453
German Terrile <gterrile@gmail.com>
2025-04-29 23:26:09 -0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import quiltyLogo from './assets/quiltycosturitas-logo.JPEG'
+import instagram from './assets/instagram.svg'
+import tiktok from './assets/tiktok.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class="layout">
+        <header class="header">
+          <div className='menu'>
+            <div className='menu-item borel-regular'>Shop coming soon</div>
+          </div>
+        </header>
+        <main class="content">
+          <img src={quiltyLogo} class="logo"/>
+          <div class="text-container">
+            <h2 class="borel-regular">my little rincon where old telas find new purpose</h2>
+          </div>
+        </main>
+        <footer class="footer">
+          <a href="https://www.tiktok.com/@quiltycosturitas">
+            <img src={tiktok} class="logo-mini"/>
+          </a>
+          <a href="https://www.instagram.com/quiltycosturitas/">
+          <img src={instagram} class="logo-mini"/>
+          </a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default App
